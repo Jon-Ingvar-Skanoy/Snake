eb36e3d301b53cfd14021ee3a98ed4ccf9773428
Jon Ingvar Jonassen Skånøy <joningvarjs@gmail.com>
2023-07-23 01:29:17 +0200

    random search

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ import sys
 import grid
 import numpy as np
 from pygame.locals import QUIT
+import copy
+import time
+import random
 
 import Apple
 import Snake
 pg.init()
-disx = 1000
-disy = 1000
+disx = 800
+disy = 800
 gridx = 18
 gridy = 18
 
@@ -32,16 +35,67 @@ snake = Snake.Snake(Dis, hexx, hexy, map_s, grid)
 map_s[round(gridx/2), round(gridx/2)] = 1
 map_s[round(gridx/5), round(gridx/2)] = 2
 
+def ai_moves( moves):
+        for move in moves:
+            snake.ai_move(move)
+            snake.plot()
+            apple.plot()
+
+            pg.display.update()
+            Dis.fill(BLACK)
+            time.sleep(0.2)
+            events = pg.event.get()
+            for event in events:
+                if event.type == QUIT:
+                    pg.quit()
+                    sys.exit()
+            
+def ai(map_s):
+    p_m = [ "Left", "Up", "Right", "Down"]
+    c = 0
+    while(1):
+        
+        moves = []
+        c += 1
+        for i in range(10):
+            tmp_1 = random.randint(0,3)
+            moves.append(p_m[tmp_1])
+
+        tmp = snake.sim_moves(moves)
+        if(type(tmp) != int and c > 5000):
+           
+            return moves[0]
+        if(type(tmp)== int):
+            if(tmp == 1):
+                if (snake.legal_move(moves[0],snake.map_s)):
+                    print(c)
+                    return moves[0]
+
+        
 while running:
-    snake.move()
-    apple.plot()
+    
+    #snake.sim_size = snake.size
+    
+    
+            
+        
+   
+   
+    
+    
+    snake.ai_move(ai(snake.map_s))
+    
     snake.plot()
+    apple.plot()
+
+    pg.display.update()
+    Dis.fill(BLACK)
+    time.sleep(0.2)
     events = pg.event.get()
     for event in events:
         if event.type == QUIT:
             pg.quit()
             sys.exit()
-            running = False
+            
 
-    pg.display.update()
-    Dis.fill(BLACK)
+   
